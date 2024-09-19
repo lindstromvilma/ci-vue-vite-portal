bd0a402c6c6f87994d9ec8845a973b65770a62a1
Vilma Lindström <vilma.lindstrom@virnex.fi>
2024-09-19 11:06:39 +0300

    Use news data from controller in home

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { ref } from 'vue'
 import Megaphone from '../Components/Icons/Megaphone.vue'
 
 const props = defineProps({
@@ -7,33 +6,29 @@ const props = defineProps({
 		type: String,
 		required: true,
 	},
+	news: {
+		type: Array,
+		default: () => [],
+	},
 })
-
-// sample news data
-const newsItems = ref([
-	{ date: '11.09.2024', news: 'Uusi Luukku avattu' },
-	{ date: '04.06.2024', news: 'Testi tiedote' },
-	{ date: '18.03.2024', news: 'Henkilöhaut toimivat' },
-	{ date: '20.02.2024', news: 'Tietokanta päivitetty' },
-])
 </script>
 
 <template>
-  <div class="flex flex-col gap-10 text-black w-full max-w-screen-xl">
+  <div class="flex flex-col gap-10 text-black w-full max-w-screen-2xl">
     <h1 class="text-2xl font-normal">
-      {{ props.message }}
+      {{ message }}
     </h1>
 
     <div class="flex flex-col gap-4 w-full">
       <div class="flex gap-2 items-center">
         <Megaphone class="h-5" />
-        <h2 class="text-xl font-medium">
+        <h2 class="text-xl font-normal">
           Ajankohtaista
         </h2>
       </div>
       <div>
         <div
-          v-for="(item, index) in newsItems"
+          v-for="(item, index) in news"
           :key="index"
           class="news-item border-b py-2"
         >
@@ -41,7 +36,7 @@ const newsItems = ref([
             {{ item.date }}
           </p>
           <p class="news text-sm font-normal">
-            {{ item.news }}
+            {{ item.title }}
           </p>
         </div>
       </div>
