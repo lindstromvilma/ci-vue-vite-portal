761a85866e0c8f560e17be0ff118fe7e1b32dff1
Vilma Lindström <vilma.lindstrom@virnex.fi>
2024-09-19 12:37:04 +0300

    Change async component to dynamic import and change some variable names

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,12 +10,14 @@ const { page, props } = appElement.dataset
 const parsedProps = JSON.parse(props) // parse the props from the dataset
 appElement.removeAttribute('data-props')
 
-// define the async component
-const DynamicComponent = defineAsyncComponent(() =>
-	import(`./Pages/${page}.vue`)
-)
-
-app.component('DynamicPage', DynamicComponent) // register the component
-
-app.provide('pageProps', parsedProps) // provide the props to the component
-app.mount('#app') // mount the app to the DOM
+// dynamic import of the page components
+import(`./Pages/${page}.vue`)
+	.then(module => {
+		const DynamicPage = module.default
+		app.component('DynamicPage', DynamicPage) // register the component
+		app.provide('pageProps', parsedProps) // Provide the props to the component
+		app.mount('#app') // Mount the app to the DOM
+	})
+	.catch(error => {
+		console.error('Error loading componen:', error)
+	})
