3214cc4eb1f3b8594be000aa13bdde25b678eed8
Vilma Lindström <vilma.lindstrom@virnex.fi>
2024-10-02 11:35:32 +0300

    Changes to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,39 +15,39 @@ module.exports = {
 				white: '#FFF',
 				'primary': {
 					DEFAULT: '#2563EB', // blue-600
-					800: '#1E40AF',    // blue-800
+					800: '#1E40AF', // blue-800
 					400: '#60A5FA', // blue-400
 					300: '#93C5FD', // blue-300
-					200: '#BFDBFE',   // blue-200
+					200: '#BFDBFE', // blue-200
 					100: '#DBEAFE', // blue-100
 					50: '#EFF6FF', // blue-50
 				},
 				'secondary': {
 					DEFAULT: '#D946EF', // fuchsia-500
-					600: '#C026D3',   // fuchsia-600
-					300: '#F0ABFC',   // fuchsia-300
-					200: '#F5D0FE',   // fuchsia-200
+					600: '#C026D3', // fuchsia-600
+					300: '#F0ABFC', // fuchsia-300
+					200: '#F5D0FE', // fuchsia-200
 				},
 				'info': {
 					DEFAULT: '#0EA5E9', // sky-500
-					600: '#0284C7',   // sky-600
-					400: '#38BDF8',   // sky-400
-					300: '#7DD3FC',   // sky-300
-					100: '#E0F2FE',   // sky-100
+					600: '#0284C7', // sky-600
+					400: '#38BDF8', // sky-400
+					300: '#7DD3FC', // sky-300
+					100: '#E0F2FE', // sky-100
 				},
 				'danger': {
 					DEFAULT: '#F43F5E', // rose-500
-					600: '#E11D48',   // rose-600
-					400: '#FB7185',   // rose-400
-					300: '#FDA4AF',   // rose-300
-					100: '#FFE4E6',   // rose-100
+					600: '#E11D48', // rose-600
+					400: '#FB7185', // rose-400
+					300: '#FDA4AF', // rose-300
+					100: '#FFE4E6', // rose-100
 				},
 				'success': {
 					DEFAULT: '#10B981', // emerald-500
-					600: '#059669',   // emerald-600
-					400: '#34D399',   // emerald-400
-					300: '#6EE7B7',   // emerald-300
-					100: '#D1FAE5',   // emerald-100
+					600: '#059669', // emerald-600
+					400: '#34D399', // emerald-400
+					300: '#6EE7B7', // emerald-300
+					100: '#D1FAE5', // emerald-100
 				},
 
 				'vtd-primary': colors.blue,
