1668e7c958f5cda4f077617ce31b7b59db2e5b51
Vilma Lindström <vilma.lindstrom@virnex.fi>
2024-10-11 12:54:31 +0300

    add simple textarea component

---
+++ resources/js/Components/TextArea.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+
+const props = defineProps({
+	placeholder: {
+		type: String,
+		default: '',
+	},
+});
+
+const model = defineModel({
+	type: [ String, Number ],
+	default: '',
+});
+
+const input = ref(null);
+
+onMounted(() => {
+	if (input.value.hasAttribute('autofocus')) {
+		input.value.focus();
+	}
+});
+
+defineExpose({ focus: () => input.value.focus() });
+</script>
+
+<template>
+  <textarea
+    ref="input"
+    v-model="model"
+    :placeholder="placeholder"
+    class="text-sm font-normal resize-y w-full px-2.5 py-2 border border-gray-300 focus:border-gray-500 outline-none focus:outline-none rounded"
+  />
+</template>
