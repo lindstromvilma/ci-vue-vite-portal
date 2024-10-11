d4f2ae57077569753310645142d4b4be8c3dfecd
Vilma Lindström <vilma.lindstrom@virnex.fi>
2024-10-11 10:56:49 +0300

    Adds arrow up-down icon

---
+++ resources/js/Components/Icons/ArrowUpDown.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg
+    data-slot="icon"
+    aria-hidden="true"
+    fill="none"
+    stroke-width="1.5"
+    stroke="currentColor"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    ></path>
+  </svg>
+</template>
