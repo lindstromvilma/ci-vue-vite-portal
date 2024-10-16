36b04e25195e9c81f483edeeb10c8b539e30da44
Vilma Lindström <vilma.lindstrom@virnex.fi>
2024-10-16 14:13:00 +0300

    remove field component and change form to just generate dynamic components

--- resources/js/Components/FormGenerator/Form.vue
+++ resources/js/Components/FormGenerator/Form.vue
@@ -1,6 +1,7 @@
 <script setup>
-import { reactive, defineAsyncComponent, onMounted } from 'vue'
-import Field from './Field.vue'
+import { reactive, ref, defineAsyncComponent, onMounted } from 'vue'
+import InputLabel from '../InputLabel.vue'
+import InputError from '../InputError.vue'
 
 const props = defineProps({
 	form: {
@@ -10,25 +11,28 @@ const props = defineProps({
 })
 
 const formData = reactive({})
+const errors = ref([])
 
 // generate form data based on fields
 const generateFormData = (fields) => {
 	fields.forEach(field => {
 		const key = field.savedTo || field.name
-		formData[key] = ''
+		formData[key] = field.type === 'multiselect' ? [] : ''
 	})
 }
 
 // component mapping for dynamic imports based on field type
 const componentMap = {
-	text: () => defineAsyncComponent(() => import('../TextInput.vue')),
-	select: () => defineAsyncComponent(() => import('../BaseSelect.vue')),
-	date: () => defineAsyncComponent(() => import('../DatePicker.vue')),
+	text: defineAsyncComponent(() => import('../TextInput.vue')),
+	select: defineAsyncComponent(() => import('../BaseSelect.vue')),
+	multiselect: defineAsyncComponent(() => import('../MultiSelect.vue')),
+	date: defineAsyncComponent(() => import('../DatePicker.vue')),
+	textarea: defineAsyncComponent(() => import('../TextArea.vue')),
 }
 
 // get component based on the field type
 const getComponent = (field) => {
-	return componentMap[field.type] ? componentMap[field.type]() : componentMap.text()
+	return componentMap[field.type] || componentMap.text
 }
 
 onMounted(() => {
@@ -44,20 +48,22 @@ onMounted(() => {
       {{ form.name }}
     </h2>
     <div class="flex flex-col gap-4">
-      <Field
+      <div
         v-for="(field, id) in form.fields"
-        :id="field.savedTo"
         :key="id"
-        :label="field.name"
       >
+        <InputLabel
+          :value="field.name"
+        />
         <component
           :is="getComponent(field)"
-          v-model="formData.savedTo"
+          v-model="formData[field.savedTo]"
           :field="field"
           :options="field.values"
           :required="field.required"
         />
-      </Field>
+        <InputError :errors="errors" />
+      </div>
     </div>
   </form>
 </template>
