acbaa094315d75fb44fa9b37192dce4377811d55
Vilma Lindström <vilma.lindstrom@virnex.fi>
2024-10-17 09:06:32 +0300

    add modal and use it in table to edit info

---
+++ resources/js/Components/BaseModal.vue
@@ -0,0 +1,30 @@
+<script setup>
+import PrimaryButton from './PrimaryButton.vue'
+
+const props = defineProps({
+	modalActive: {
+		type: Boolean,
+		required: true,
+	},
+})
+</script>
+
+<template>
+  <div
+    v-show="modalActive"
+    class="fixed inset-0 bg-black bg-opacity-20 flex items-center justify-center z-50"
+  >
+    <div
+      v-if="modalActive"
+      class="flex flex-col gap-4 bg-white px-6 py-8 mb-60 rounded shadow-md max-w-screen-md w-full"
+    >
+      <slot />
+      <PrimaryButton
+        class="self-end"
+        @click="$emit('close')"
+      >
+        Sulje
+      </PrimaryButton>
+    </div>
+  </div>
+</template>
--- resources/js/Pages/Persons.vue
+++ resources/js/Pages/Persons.vue
@@ -1,7 +1,11 @@
 <script setup>
+import { ref, watch } from 'vue'
 import SimpleTabs from '@/components/SimpleTabs.vue'
 import Table from '@/Components/Table.vue'
 import TableWithSorting from '@/Components/TableWithSorting.vue'
+import BaseModal from '@/Components/BaseModal.vue'
+import InputLabel from '@/Components/InputLabel.vue'
+import TextInput from '@/Components/TextInput.vue'
 
 const props = defineProps({
 	message: {
@@ -13,6 +17,24 @@ const props = defineProps({
 		default: () => [],
 	},
 })
+
+const modalActive = ref(false)
+const selectedRow = ref(null)
+
+const toggleModal = () => {
+	modalActive.value = !modalActive.value
+}
+
+const handleRowActionClick = ({ action, row }) => {
+	if (action === 'edit') {
+		selectedRow.value = { ...row }
+		toggleModal()
+	}
+}
+
+const closeModal = () => {
+	modalActive.value = false
+}
 
 const tabs = [
 	{ label: 'Tavallinen taulukko', value: 'basic-table' },
@@ -31,7 +53,40 @@ const tabs = [
         <Table :data="persons" />
       </template>
       <template #sortable-table>
-        <TableWithSorting :data="persons" />
+        <TableWithSorting
+          :data="persons"
+          @row-action-click="handleRowActionClick"
+        />
+        <BaseModal
+          :modal-active="modalActive"
+          @close="closeModal"
+        >
+          <h1 class="text-2xl font-semibold mb-4">
+            Muokkaa tietoja
+          </h1>
+          <div>
+            <InputLabel
+              for="email"
+              value="Uusi sähköposti"
+            />
+            <TextInput
+              id="email"
+              v-model="selectedRow.email"
+              placeholder="Kirjoita sähköposti"
+            />
+          </div>
+          <div>
+            <InputLabel
+              for="phone"
+              value="Uusi puhelinnumero"
+            />
+            <TextInput
+              id="phone"
+              v-model="selectedRow.phone"
+              placeholder="Kirjoita puhelinnumero"
+            />
+          </div>
+        </BaseModal>
       </template>
     </SimpleTabs>
   </div>
